7609413351793941960175a8bc05bfb824106fa4
Safwen Naimi <safwen.naimi@gmail.com>
2025-05-14 15:43:29 -0400

    Update fusion.py

--- fusion.py
+++ fusion.py
@@ -1,13 +1,18 @@
 from mmcv import load, dump
 from atemgcn.smp import *
 
+
 joint = load('work_dirs/ntu60_xsub/j/result.pkl')
 bone = load('work_dirs/ntu60_xsub/b/result.pkl')
-#joint_motion = load('jm.pkl')
-#bone_motion = load('bm.pkl')
+joint_motion = load('work_dirs/ntu60_xsub/jm/result.pkl')
+bone_motion = load('work_dirs/ntu60_xsub/bm/result.pkl')
+
 label = load_label('data/ntu60.pkl', 'xsub_val')
 
+# J+B Fusion result
 fused = comb([joint, bone], [1, 1])
 print('Top-1', top1(fused, label))
-#fused = comb([joint, bone, joint_motion, bone_motion], [2, 2, 1, 1])
-#print('Top-1', top1(fused, label))
+
+# J+B+JM+BM Fusion result
+fused = comb([joint, bone, joint_motion, bone_motion], [2, 2, 1, 1])
+print('Top-1', top1(fused, label))
