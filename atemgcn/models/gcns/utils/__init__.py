d0c06cd55b145a621c9847f060eb1c3e0d1baa3e
Safwen Naimi <safwen.naimi@gmail.com>
2025-05-15 14:08:35 -0400

    Update __init__.py

--- atemgcn/models/gcns/utils/__init__.py
+++ atemgcn/models/gcns/utils/__init__.py
@@ -1,6 +1,6 @@
 from .gcn import dggcn, unit_aagcn, unit_ctrgcn, unit_gcn, unit_sgn
 from .init_func import bn_init, conv_branch_init, conv_init
-from .atemgcn_utils import MSGCN, MSTCN, MW_MSG3DBlock
+from .atemgcn_utils import DUMA, ATEM, MW_ATEMGCNBlock
 from .tcn import dgmstcn, mstcn, unit_tcn
 
 __all__ = [
@@ -9,7 +9,7 @@ __all__ = [
     # TCN Modules
     'unit_tcn', 'mstcn', 'dgmstcn',
     # ATEMGCN Utils
-    'MSGCN', 'MSTCN', 'MW_ATEMGCNBlock',
+    'DUMA', 'ATEM', 'MW_ATEMGCNBlock',
     # Init functions
     'bn_init', 'conv_branch_init', 'conv_init'
 ]
