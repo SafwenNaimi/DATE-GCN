c8301b9ab534290447bacf323caaf8fabf6a52b4
Safwen Naimi <safwen.naimi@gmail.com>
2025-05-15 14:10:12 -0400

    Update __init__.py

--- atemgcn/models/__init__.py
+++ atemgcn/models/__init__.py
@@ -1,6 +1,4 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 from .builder import *  # noqa: F401, F403
-from .cnns import *  # noqa: F401, F403
 from .gcns import *  # noqa: F401, F403
 from .heads import *  # noqa: F401, F403
 from .losses import *  # noqa: F401, F403
